6bbd6b92418b905a1d1e2373a4e858cb82adc699
Ali Tarık <alimirik55@gmail.com>
2022-12-05 13:30:44 +0300

    create favorite functions

--- server/controller/user.js
+++ server/controller/user.js
@@ -5,17 +5,40 @@ module.exports = {
     getFavs: async (req, res) => {
         let db_connect = dbo.getDb("runtime-hunter");
 
-        db_connect.collection("users").findOne({ "email": req.body.email })
-            .then((result) => {
-                return result.favs ?? []
-            })
+        const users = db_connect.collection('users');
+
+        // Get the user's favorites from the database
+        const user = await users.findOne({ _id: userId });
+
+        // Get the details of each favorite item
+        const favorites = [];
+        for (const favoriteId of user.favorites) {
+            const items = db.collection('items');
+            const favorite = await items.findOne({ _id: favoriteId });
+            favorites.push(favorite);
+        }
+
+        return favorites;
     },
     addToFavs: async (req, res) => {
-        let db_connect = dbo.getDb("runtime-hunter");
+        let db = dbo.getDb("runtime-hunter");
 
-        db_connect.collection("users").update({ "email": req.body.email }, { $addToSet: { favs: req.body.courseId } })
+        const users = db.collection('users');
+
+        // Add the item to the user's favorites in the database
+        await users.updateOne(
+            { _id: userId },
+            { $addToSet: { favorites: item } }
+        );
     },
     removeFromFavs: async (req, res) => {
+        let db = dbo.getDb("runtime-hunter");
+        const users = db.collection('users');
 
+        // Remove the item from the user's favorites in the database
+        await users.updateOne(
+            { _id: userId },
+            { $pull: { favorites: item } }
+        );
     }
 }
